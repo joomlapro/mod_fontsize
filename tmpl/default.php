a1dc990ed74088f5101f0a1c055c4e7691fa7a0e
Bruno Batista <bruno@brunobatista.eti.br>
2013-09-11 11:35:19 -0300

    Refactoring of default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -17,20 +17,27 @@ JHtml::stylesheet('mod_fontsize/template.css', false, true, false);
 // Load JavaScript.
 JHtml::script('mod_fontsize/store.min.js', false, true);
 JHtml::script('mod_fontsize/rv-jquery-fontsize.min.js', false, true);
+
+// Get the document object.
+$doc = JFactory::getDocument();
+
+// Build the script.
+$script = array();
+$script[] = 'jQuery(document).ready(function($) {';
+$script[] = '  $.rvFontsize({';
+$script[] = '    targetSection: \'' . $targetSection . '\',';
+$script[] = '    store: ' . $store . ',';
+$script[] = '    controllers: {';
+$script[] = '      appendTo: \'' . $appendTo . '\',';
+$script[] = '      showResetButton: ' . $showResetButton . ',';
+$script[] = '      template: \'<li><a href="#" class="rvfs-decrease" data-toggle="tooltip" data-placement="bottom" title="' . JText::_('MOD_FONTSIZE_FONT_DECREASE') . '"><i class="icon-font"></i>-</a></li>\'';
+$script[] = '        + \'<li><a href="#" class="rvfs-reset" data-toggle="tooltip" data-placement="bottom" title="' . JText::_('MOD_FONTSIZE_FONT_RESET') . '"><i class="icon-font"></i></a></li>\'';
+$script[] = '        + \'<li><a href="#" class="rvfs-increase" data-toggle="tooltip" data-placement="bottom" title="' . JText::_('MOD_FONTSIZE_FONT_INCREASE') . '"><i class="icon-font"></i>+</a></li>\'';
+$script[] = '    }';
+$script[] = '  });';
+$script[] = '});';
+
+// Add the script to the document head.
+$doc->addScriptDeclaration(implode("\n", $script));
 ?>
-<script type="text/javascript">
-	jQuery(document).ready(function($) {
-		$.rvFontsize({
-			targetSection: '<?php echo $targetSection; ?>',
-			store: <?php echo $store; ?>,
-			controllers: {
-				appendTo: '<?php echo $appendTo; ?>',
-				showResetButton: <?php echo $showResetButton; ?>,
-				template: '<li><a href="#" class="rvfs-decrease" data-toggle="tooltip" data-placement="bottom" title="<?php echo JText::_('MOD_FONTSIZE_FONT_DECREASE'); ?>"><i class="icon-font"></i>-</a></li>'
-					+ '<li><a href="#" class="rvfs-reset" data-toggle="tooltip" data-placement="bottom" title="<?php echo JText::_('MOD_FONTSIZE_FONT_RESET'); ?>"><i class="icon-font"></i></a></li>'
-					+ '<li><a href="#" class="rvfs-increase" data-toggle="tooltip" data-placement="bottom" title="<?php echo JText::_('MOD_FONTSIZE_FONT_INCREASE'); ?>"><i class="icon-font"></i>+</a></li>'
-			}
-		});
-	});
-</script>
 <ul id="rvfs-controllers" class="fontsize-controllers list-unstyled<?php echo $moduleclass_sfx; ?>"></ul>
